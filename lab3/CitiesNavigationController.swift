90d59f1e58fa99ba5f008c3aead72282eb885eda
vanashimko <vana.shimko@gmail.com>
2017-03-07 16:44:01 +0300

    Fix model injection to cities table view

---
+++ lab3/CitiesNavigationController.swift
@@ -0,0 +1,26 @@
+//
+//  CitiesNavigationController.swift
+//  lab3
+//
+//  Created by Admin on 07.03.17.
+//  Copyright © 2017 Konstantin Terehov. All rights reserved.
+//
+
+import UIKit
+
+class CitiesNavigationController: UINavigationController, WeatherModelInjectable {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    func setWeatherModel(weatherModel: WeatherModel) {
+        for viewController in self.childViewControllers {
+            if let weatherModelInjectable = viewController as? WeatherModelInjectable {
+                weatherModelInjectable.setWeatherModel(weatherModel: weatherModel)
+            }
+        }
+    }
+}
